6fbaf05a60ff32422d202a638040871c2989072d
John Vandenberg <jayvdb@gmail.com>
2020-06-11 22:58:03 +0700

    Expand test harness

--- test_settings.py
+++ test_settings.py
@@ -1,6 +1,13 @@
+import os.path
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 INSTALLED_APPS = ('typedmodels', 'django.contrib.contenttypes')
 MIDDLEWARE_CLASSES = ()
 DATABASES = {
-    'default': {'ENGINE': 'django.db.backends.sqlite3'}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 SECRET_KEY = 'abc123'
